110df4e9ba239f4b4fb8f3eaa07f44c2cc03097f
Сергей <@ANDREYNOTEBOOK>
2022-10-31 22:19:13 +0300

    Добавьте файлы проекта.

---
+++ 1.33/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+double people, area;
+Console.WriteLine("Ввидите число насиления");
+people = int.Parse(Console.ReadLine());
+Console.WriteLine("Ввидите площадь страны");
+area = int.Parse(Console.ReadLine());//  ввод значений
+double result;
+result = people / area;
+Console.WriteLine(" Ответ {0} на км в квадрате",result);
